ad8ca00e3a93a77d2744afe7a4e70d22b99bd5c1
aquick05 <aurelien05@gmail.com>
2025-10-27 12:09:12 +0100

    tabbar

--- App.js
+++ App.js
@@ -14,8 +14,8 @@ import ProfileScreen from "./screens/ProfileScreen";
 import user from "./reducers/user";
 import { Provider } from "react-redux";
 import { persistStore, persistReducer } from "redux-persist";
-
 import { PersistGate } from "redux-persist/integration/react";
+//async storage pour react-native car lacal storage ne fonctione pas
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { useSelector } from "react-redux";
@@ -46,7 +46,21 @@ const TabNavigator = () => {
           } else if (route.name === "Profil") {
             iconName = "person-outline";
           } else if (route.name === "add") {
-            iconName = "add-circle-outline";
+            //Style personnalisé pour le bouton Add
+            return (
+              <View
+                style={{
+                  width: 60,
+                  height: 60,
+                  borderRadius: 30,
+                  backgroundColor: "#8E7EED",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <Ionicons name="add" size={32} color="white" />
+              </View>
+            );
           }
 
           return <Ionicons name={iconName} size={size} color={color} />;
@@ -58,13 +72,21 @@ const TabNavigator = () => {
     >
       <Tab.Screen name="Acceuil" component={HomeScreen} />
       <Tab.Screen name="calendrier" component={CalendarScreen} />
-      <Tab.Screen name="add" component={AddScreen} />
+      <Tab.Screen
+        name="add"
+        component={AddScreen}
+        options={{
+          tabBarLabel: "", // Pas de label pour le bouton add
+        }}
+      />
       <Tab.Screen name="famille" component={FamillyScreen} />
       <Tab.Screen name="Profil" component={ProfileScreen} />
     </Tab.Navigator>
   );
 };
-const RootNavigator = () => {
+
+// affichage de authScreen si non connecté sinon arrivé sur homeScreen
+const DisplayIsLogged = () => {
   const userData = useSelector((state) => state.user.value);
   console.log(userData?.isLogged);
   return (
@@ -83,7 +105,7 @@ export default function App() {
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor}>
-        <RootNavigator />
+        <DisplayIsLogged />
       </PersistGate>
     </Provider>
   );
