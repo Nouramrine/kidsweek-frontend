a33f41aa576b220af115eaf2ab717719734c9ac0
aquick05 <aurelien05@gmail.com>
2025-10-28 15:54:05 +0100

    synchronisation bdd reducer pour signin et signup

--- screens/_partials/SignIn.js
+++ screens/_partials/SignIn.js
@@ -1,39 +1,30 @@
-import React, { useState } from 'react';
-import { View, TextInput, Button, StyleSheet } from 'react-native';
-import { useDispatch } from 'react-redux';
+import React, { useState } from "react";
+import { View, TextInput, Button, StyleSheet } from "react-native";
+import { useDispatch } from "react-redux";
 import { login } from "../../reducers/user";
-import KWTextInput from '../../components/KWTextInput';
-import KWButton from '../../components/KWButton';
-import KWText from '../../components/KWText';
-
+import KWTextInput from "../../components/KWTextInput";
+import KWButton from "../../components/KWButton";
+import KWText from "../../components/KWText";
+import { signinAsync } from "../../reducers/user";
 const BACKEND_URL = process.env.EXPO_PUBLIC_API_URL;
 
 const SignIn = () => {
-  const [signInEmail, setSignInEmail] = useState('user@kidsweek.fr');
-  const [signInPassword, setSignInPassword] = useState('Pass1234');
-  const [signInError, setSignInError] = useState('');
+  const [signInEmail, setSignInEmail] = useState("user@kidsweek.fr");
+  const [signInPassword, setSignInPassword] = useState("Pass1234");
+  const [signInError, setSignInError] = useState("");
 
-
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   const handleSignIn = () => {
-		fetch(`${BACKEND_URL}/members/signin`, {
-			method: 'POST',
-			headers: { 'Content-Type': 'application/json' },
-			body: JSON.stringify({ email: signInEmail, password: signInPassword }),
-		}).then(response => response.json())
-			.then(data => {
-				if (data.result) {
-          const { firstName, lastName, email, token } = data.member;
-					dispatch(login({ firstName, lastName, email, token }));
-					setSignInEmail('');
-					setSignInPassword('');
-				} else {
-          setSignInError(data.error)
-        }
-			})
-      .catch((error) => console.log(error));
-	};
+    dispatch(
+      signinAsync({
+        email: signInEmail,
+        password: signInPassword,
+      })
+    );
+    setSignInEmail("");
+    setSignInPassword("");
+  };
 
   return (
     <View>
@@ -50,7 +41,7 @@ const SignIn = () => {
         value={signInPassword}
         onChangeText={setSignInPassword}
       />
-      <KWText type='inputError'>{signInError}</KWText>
+      <KWText type="inputError">{signInError}</KWText>
       <KWButton title="Se connecter" onPress={handleSignIn} />
     </View>
   );
