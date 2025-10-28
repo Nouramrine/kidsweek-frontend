66d28d0937841171ddb55ef42e4ddab435e88519
JerGuer <jeremy.guerlin@gmail.com>
2025-10-28 14:23:56 +0100

    family page

--- screens/FamillyScreen.js
+++ screens/FamillyScreen.js
@@ -7,6 +7,25 @@ import KWText from "../components/KWText";
 import { FontAwesome5 } from '@expo/vector-icons';
 
 const FamillyScreen = (navigation) => {
+
+  const membersData = [
+    { id: "658961", firstName: "Papa", lastName: "Outé"},
+    { id: "552638", firstName: "Maman", lastName: "Bobo"},
+    { id: "552638", firstName: "Mamie", lastName: "Nova"},
+    { id: "12356", firstName: "Lucas", lastName: "Rabine"},
+    { id: "78569", firstName: "Anna", lastName: "Lefabète"},
+  ]
+
+  const zonesData = [
+    { name: "Papa", color: "blue", members: ["658961", "12356"]},
+    { name: "Maman", color: "red", members: ["552638","12356","78569"]},
+    { name: "Mamie", color: "purple", members: ["12356","78569"]},
+
+  ]
+
+  const [zones, setZones] = useState(zonesData);
+  const [members, setMembers] = useState(membersData);
+  
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <View style={styles.container}>
@@ -26,6 +45,24 @@ const FamillyScreen = (navigation) => {
             </KWCard>
           </View>
         </View>
+      
+        <View style={styles.zonesContainer}>
+            {zones.map((zone) => (
+              <KWCard color={zone.color} style={styles.zoneCard}>
+                <View style={styles.cardHeader}>
+                  <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+                    <View style={{ backgroundColor: colors[zone.color + '3'], padding: 10, borderRadius: 10 }}>
+                      <FontAwesome5 name="home" size={24} color="white" />
+                    </View>
+                    <View>
+                      <KWText>{zone.name}</KWText>
+                      <KWText>{zone.members.length}</KWText>
+                    </View>
+                  </View>
+                </View>
+              </KWCard>              
+            ))}
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -38,6 +75,10 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "flex-start",
     alignItems: "center",
+    padding: 10,
+  },
+  zonesContainer: {
+    width: '100%',
     padding: 10,
   },
   topButtonsContainer: {
@@ -53,5 +94,13 @@ const styles = StyleSheet.create({
   },
   buttonSubTitle: {
     fontSize: 12,
-  }
+  },
+  zoneCard: {
+    marginBottom: 20,
+  },
+  cardHeader: {
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'space-between',
+  },
 });
