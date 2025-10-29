df98006b5bb48f8a71c6435ba02ceddd782d1786
JerGuer <jeremy.guerlin@gmail.com>
2025-10-29 14:09:06 +0100

    family modif

--- theme/colors.js
+++ theme/colors.js
@@ -4,10 +4,10 @@ export const colors = {
     blue: ['#DBEAFE','#63a7b8','#337a8b'],
     green: ['#DBFCE7','#5cb9b2','#308b84' ],
     purple: ['#EDE9FE', '#624fceff', '#3622a8ff'],
-    orange: ['#ffe1d0', '#ffe1d0', '#ffe1d0'],
+    orange: ['#ffe1d0', '#faaf83ff', '#e97a3aff'],
     pink: ['#fcd8ec', '#ffb0dc', '#ff84c8'],
-    red: ['#fcd8d8', '#ffb4b4ff', '#fc8585ff'],
+    red: ['#fcd8d8', '#ff7c7c', '#da3939'],
     yellow: ['#FEF9C2','#FFF085','#a65f00'],
     skin: ['#fcf0e6'],
-    error: ['#8b2020'],
+    error: ['#9c3131'],
 }
