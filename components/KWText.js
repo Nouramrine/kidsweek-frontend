7f4e8f6a874521a714741e2bf18b5e1b46f19f8d
aquick05 <aurelien05@gmail.com>
2025-10-29 09:53:04 +0100

    sytle adscreen

--- components/KWText.js
+++ components/KWText.js
@@ -1,45 +1,41 @@
-import { Text, StyleSheet } from 'react-native';
-import { colors } from '../theme/colors';
+import { Text, StyleSheet } from "react-native";
+import { colors } from "../theme/colors";
 
 const KWText = ({ children, ...props }) => {
-    const type = props?.type || 'text'
-    const color = props?.color || 'text'
-    return (
-        <Text style={[styles.text, styles[type], props.style, { color: color }]}>{children}</Text>
-    );
+  const type = props?.type || "text";
+  const color = props?.color || "text";
+  return (
+    <Text style={[styles.text, styles[type], props.style, { color: color }]}>
+      {children}
+    </Text>
+  );
 };
 
 const styles = StyleSheet.create({
-    text: {
-        fontSize: 16,
-        fontFamily: 'JosefinSans_400Regular',
-    },
-    text: {
-        fontSize: 16,
-        fontFamily: 'JosefinSans_400Regular',
-    },
-    h1: {
-        fontSize: 28,
-        fontFamily: 'Gluten_500Medium',
-        textAlign: 'center',
-        padding: 10,
-    },
-    h2: {
-        fontSize: 18,
-        fontFamily: 'Gluten_500Medium',
-        textAlign: 'left',
-        padding: 10,
-    },
-    h3: {
-
-    },
-    inputError: {
-        color: colors.error,
-        paddingBottom: 10,
-        paddingHorizontal: 15,
-        fontWeight: 'bold',
-        fontSize: 12,
-    }
+  text: {
+    fontSize: 16,
+    fontFamily: "JosefinSans_400Regular",
+  },
+  h1: {
+    fontSize: 28,
+    fontFamily: "Gluten_500Medium",
+    textAlign: "center",
+    padding: 10,
+  },
+  h2: {
+    fontSize: 18,
+    fontFamily: "Gluten_500Medium",
+    textAlign: "left",
+    padding: 10,
+  },
+  h3: {},
+  inputError: {
+    color: colors.error,
+    paddingBottom: 10,
+    paddingHorizontal: 15,
+    fontWeight: "bold",
+    fontSize: 12,
+  },
 });
 
 export default KWText;
