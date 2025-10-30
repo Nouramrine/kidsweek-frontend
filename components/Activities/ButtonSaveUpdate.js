be4be67720ec9ef654682d757f11f59da195a3a5
aquick05 <aurelien05@gmail.com>
2025-10-30 17:56:02 +0100

    verification formulaire

--- components/Activities/ButtonSaveUpdate.js
+++ components/Activities/ButtonSaveUpdate.js
@@ -9,7 +9,7 @@ const ButtonSaveUpdate = ({
   props,
 }) => {
   // Si on est en mode "mise à jour"
-  console.log("ButtonSaveUpdate props:", props);
+
   if (props && Object.keys(props).length !== 0) {
     return (
       <KWButton title="Mettre à jour" type="text" onPress={handleUpdate} />
