b843ebb09cce1fa9d620a5157b6cda75d56eca5b
aquick05 <aurelien05@gmail.com>
2025-11-05 17:19:53 +0100

    mon vvvv

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,4 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  env: {
-    test: {
-      plugins: ['@babel/plugin-transform-modules-commonjs'],
-    },
-  },
+  presets: ["babel-preset-expo"],
+  plugins: [],
 };
