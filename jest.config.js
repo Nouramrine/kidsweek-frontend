f4dc1b10370ce059bcb08959a0870aacb53edc2b
JerGuer <jeremy.guerlin@gmail.com>
2025-11-05 14:36:49 +0100

    adding tests

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,9 @@ module.exports = {
   preset: 'react-native',
   setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
   transformIgnorePatterns: [
+    // Transformer react-native et ses dépendances internes
     "node_modules/(?!(react-native|@react-native|@react-navigation|react-native-vector-icons|react-native-safe-area-context)/)"
   ],
-  // Utiliser babel-jest pour tout JS/TS
   transform: {
     "^.+\\.(js|jsx|ts|tsx)$": "babel-jest"
   },
