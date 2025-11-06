990ed5022ffd907e0096e79b429d5896be597c15
aquick05 <aurelien05@gmail.com>
2025-11-06 08:45:47 +0100

    ok

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
-module.exports = {
-  presets: ["babel-preset-expo"],
-  plugins: [],
-};
-=======
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
   };
 };
->>>>>>> f963ca5dc9e27048a6144649e34cdc8d3bb32c18
