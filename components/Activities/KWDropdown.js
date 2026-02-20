5303d6c395d4615b586930c920ebfbe620899b33
Nour <nouramrine@gmail.com>
2026-02-20 14:12:49 +0100

    fix(ui): fix password visibility and reminder dropdown text color

--- components/Activities/KWDropdown.js
+++ components/Activities/KWDropdown.js
@@ -1,5 +1,6 @@
 import { Picker } from "@react-native-picker/picker";
 import { StyleSheet } from "react-native";
+import { colors } from "../../theme/colors";
 
 const KWDropdown = ({ selectedValue, onValueChange, style }) => {
   return (
@@ -7,10 +8,12 @@ const KWDropdown = ({ selectedValue, onValueChange, style }) => {
       selectedValue={selectedValue}
       onValueChange={onValueChange}
       style={[styles.picker, style]}
+      dropdownIconColor={colors.text[0]}
+      mode="dropdown"
     >
-      <Picker.Item label="minutes" value="minutes" />
-      <Picker.Item label="heures" value="heures" />
-      <Picker.Item label="jours" value="jours" />
+      <Picker.Item label="minutes" value="minutes" color={colors.text[0]} />
+      <Picker.Item label="heures" value="heures" color={colors.text[0]} />
+      <Picker.Item label="jours" value="jours" color={colors.text[0]} />
     </Picker>
   );
 };
@@ -18,6 +21,7 @@ const KWDropdown = ({ selectedValue, onValueChange, style }) => {
 const styles = StyleSheet.create({
   picker: {
     height: 50,
+    color: colors.text[0],
   },
 });
 
--- components/KWTextInput.js
+++ components/KWTextInput.js
@@ -1,54 +1,71 @@
-import { Text, View, TextInput, StyleSheet } from 'react-native';
-import KWText from '../components/KWText';
+import { Text, View, TextInput, StyleSheet } from "react-native";
+import KWText from "../components/KWText";
 import { colors } from "../theme/colors";
 
-const KWTextInput = ({ label, ...props }) => {
-    return (
-        <View style={styles.container}>
-            <Text style={[styles.label, props.error && { color: colors.error[0] }]}>{label}</Text>
-            <View style={[styles.inputWrapper, props.error && { borderColor: colors.error[0] } ]}>
-                <TextInput {...props} style={styles.textInput} />
-            </View>
-            {props.error && <KWText type="inputError" style={styles.inputError}>{props.error}</KWText>}
-        </View>
-    );
+const KWTextInput = ({ label, style, ...props }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={[styles.label, props.error && { color: colors.error[0] }]}>
+        {label}
+      </Text>
+      <View
+        style={[
+          styles.inputWrapper,
+          props.error && { borderColor: colors.error[0] },
+        ]}
+      >
+        <TextInput
+          {...props}
+          style={[styles.textInput, style]}
+          placeholderTextColor={colors.text[1]}
+        />
+      </View>
+      {props.error && (
+        <KWText type="inputError" style={styles.inputError}>
+          {props.error}
+        </KWText>
+      )}
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        minWidth: 150,
-        marginVertical: 10,
-        position: 'relative',
-    },
-    label: {
-        position: 'absolute',
-        top: -10,
-        left: 15,
-        backgroundColor: colors.background[0],
-        paddingHorizontal: 5,
-        color: colors.input[0],
-        fontWeight: 'bold',
-        zIndex: 1,
-    },
-    inputWrapper: {
-        borderRadius: 10,
-        borderWidth: 2,
-        borderColor: colors.input[0],
-    },
-    textInput: {
-        padding: 15,
-        paddingTop: 15,
-    },
-    inputError: {
-        position: 'absolute',
-        bottom: -15,
-        right: 15,
-        backgroundColor: colors.background[0],
-        paddingHorizontal: 5,
-        color: colors.error[0],
-        fontWeight: 'bold',
-        zIndex: 1,
-    },
+  container: {
+    minWidth: 150,
+    marginVertical: 10,
+    position: "relative",
+  },
+  label: {
+    position: "absolute",
+    top: -10,
+    left: 15,
+    backgroundColor: colors.background[0],
+    paddingHorizontal: 5,
+    color: colors.input[0],
+    fontWeight: "bold",
+    zIndex: 1,
+  },
+  inputWrapper: {
+    borderRadius: 10,
+    borderWidth: 2,
+    borderColor: colors.input[0],
+  },
+  textInput: {
+    padding: 15,
+    paddingTop: 15,
+    color: colors.text[0],
+    fontSize: 16,
+  },
+  inputError: {
+    position: "absolute",
+    bottom: -15,
+    right: 15,
+    backgroundColor: colors.background[0],
+    paddingHorizontal: 5,
+    color: colors.error[0],
+    fontWeight: "bold",
+    zIndex: 1,
+  },
 });
 
 export default KWTextInput;
